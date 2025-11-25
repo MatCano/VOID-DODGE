ba739c3e01e81b93dbc7f590a45887d95ba068ee
jc <joaotrin1@outlook.com>
2025-11-25 10:47:02 -0300

    fix: dificuldade não aumenta durante tela inicial

--- scripts/difficulty_controller.gd
+++ scripts/difficulty_controller.gd
@@ -3,6 +3,15 @@ extends Node
 @export var difficulty := 1.0				# starts at normal speed
 @export var difficulty_increase := 0.25		# how fast it ramps up
 @export var max_difficulty := 25.0			# prevents insane speeds
+@onready var game_manager = get_node("/root/Main") ####################
+
+####################
+func _ready():
+	game_manager = get_node("/root/Main") # adjust path
+####################
 
 func _process(delta: float) -> void:
+	if not game_manager.game_started:
+		return  # stop moving until SPACE
+
 	difficulty = min(difficulty + difficulty_increase * delta, max_difficulty)
